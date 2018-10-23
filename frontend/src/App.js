584e20596c5ca91568be0764cdb477945d06e612
matthew voorhees <voorhees.matthew@gmail.com>
2018-10-23 13:15:05 -0700

    Add map structures

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import MapContainer from './components/map_container';
+// import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-class App extends Component {
+
+class App extends React.Component {
   render() {
     return (<div>
       test
+      {/* <MapContainer/> */}
         </div>);
       }
 }
