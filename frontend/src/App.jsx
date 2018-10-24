ec9b72bc1cffead5f2ad3f48b58be3eda0774b7e
Kee <keejuan05@gmail.com>
2018-10-23 18:15:16 -0700

    Add successful API call to skyscanner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Axios from 'axios';
-import FlightShowContainer from './component/flight/flight_show_container.jsx';
+import FlightShowContainer from './components/flight/flight_show_container.jsx';
 
 class App extends Component {
 
 
   render() {
-
-    //https://www.html5rocks.com/en/tutorials/cors/
-      // const url = "http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/usd/en-US/us/us/anytime/anytime?apikey=prtl6749387986743898559646983194";
 
     return (
 
