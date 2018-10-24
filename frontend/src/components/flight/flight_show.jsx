e27a4d7f388e5ed29fa4494d75998601d9349051
Kee <keejuan05@gmail.com>
2018-10-24 01:10:24 -0700

    Add 2 input fields and change API call based on input field

--- frontend/src/components/flight/flight_show.jsx
+++ frontend/src/components/flight/flight_show.jsx
@@ -8,29 +8,50 @@ class FlightShow extends React.Component {
 
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {
+            origin: "",
+            amount: null
+        };
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount(){
          
     }
 
+    handleChange(e){
+        //handles change for fields in the form.
+        this.setState({[e.target.name]: e.target.value})
+    }
 
     handleSubmit(e){
         e.preventDefault();
-        const url = `http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/usd/en-US/us/us/anytime/anytime?apikey=prtl6749387986743898559646983194`;
-        // const url = "http://localhost:5000/all/test";
+        // need to refactor to call thunk actions
+
+        let origin = this.state.origin;
+        // const url = `http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/usd/en-US/us/us/anytime/anytime?apikey=prtl6749387986743898559646983194`;
+        const url = `http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/usd/en-US/${origin}/us/anytime/anytime?apikey=prtl6749387986743898559646983194`;
+
         Request.get(url)
           .then(data => console.log(data))
           .then(console.log("Calling api."))
           .catch(err => console.log(err));
-
     }
 
     render() {
 
-        return <button onClick={this.handleSubmit}>myButton</button>;
+        return <form onSubmit={this.handleSubmit}>
+            From: <br />
+            <input name="origin" onChange={this.handleChange} type="text" />
+            <br />
+            <br />
+            Budget: <br />
+            <input name="amount" onChange={this.handleChange} type="text" />
+            <br />
+            <br />
+            <button>Budget me!</button>
+          </form>;   
     }
 
 }
