efc00cf344cb6b16ca151936faf4bc1aaa00446c
matthew voorhees <voorhees.matthew@gmail.com>
2018-10-26 15:17:26 -0700

    Prep for API Refactor

--- frontend/src/config/keys.js
+++ frontend/src/config/keys.js
@@ -1,6 +1,5 @@
 if (process.env.NODE_ENV === "production") {
-  console.log("I'm in production")
-  module.exports = require("./keys_prod");
+  module.exports = require("./keys_prod")
 } else {
   module.exports = require("./keys_dev");
 }
